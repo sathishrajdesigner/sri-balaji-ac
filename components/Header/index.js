b707f0f9f493aa68ca319a61991e26c5147504e0
sathishrajdesigner <sathishrajdesigner@gmail.com>
2024-02-27 18:32:21 +0530

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -174,7 +174,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("tel:09786001884")}>
               Contact
             </Button>
 
